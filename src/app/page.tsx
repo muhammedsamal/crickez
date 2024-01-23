2a32dfb772b3c91ff12c849b217b3433dc5cedfe
Muhammed Samal <muhammedsamalt@gmail.com>
2024-01-23 13:20:18 +0000

    fixed some styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,20 @@ import Link from "next/link";
 export default function Home() {
   const [isHovered, setIsHovered] = useState(false);
 
+  const style =
+    "border border-green-500  text-green-500 font-bold py-2 px-4 rounded w-[220px] hover:bg-green-500 hover:text-white transition-all duration-300 ease-in-out";
+
   return (
-    <div className="flex w-screen h-screen items-center justify-center flex-col text-center to-white">
-      <h1 className="text-4xl md:text-8xl font-bold">
+    <div className="flex w-screen h-screen items-center justify-center flex-col text-center bg-white">
+      <h1 className="text-4xl md:text-8xl font-bold text-green-600">
         Welcome To World Cricket
       </h1>
       <div className="flex gap-2 items-center mt-4 flex-wrap justify-center">
         <Link href="/play">
-          <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-[220px]">
-            Play Agains Computer
-          </button>
+          <button className={style}>Play Agains Computer</button>
         </Link>
         <button
-          className={`bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-[220px] ${
-            isHovered ? "hovered" : ""
-          }`}
+          className={`${style} ${isHovered ? "hovered" : ""}`}
           title="Coming Soon..."
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
